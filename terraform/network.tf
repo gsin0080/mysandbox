d01e467975f130b95fa845944e24011362d2bf8f
Gary Sin <gary.sin@sasit.co.nz>
2022-04-30 17:36:41 +1200

    .

--- terraform/network.tf
+++ terraform/network.tf
@@ -12,7 +12,7 @@ resource "aws_vpc" "mysandbox" {
 resource "aws_subnet" "public" {
     count = var.az_count
     cidr_block = cidrsubnet(aws_vpc.mysandbox,8,count.index)
-    availability_zone = data.aws_availability_zones.names[count.index]
+    availability_zone = data.aws_availability_zones.available.names[count.index]
     vpc_id = aws_vpc.mysandbox.id
     map_public_ip_on_launch = true
 }
