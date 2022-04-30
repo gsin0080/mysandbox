007dd5389b716dc5374b9a7fd8f897b1738f77db
Gary Sin <gary.sin@sasit.co.nz>
2022-04-30 17:22:41 +1200

    .

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,4 +2,6 @@
 # Note: change to profile instead of keys for security reasons
 provider "aws" {
     region = var.aws_region
+    access_key = var.aws_access_key
+    secret_key = var.aws_secret_key
 }
